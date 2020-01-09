19ad5a1c992f4b98943c0f9faf042854b68cf818
eroji <jimmy.chen@outlook.com>
2020-01-08 16:46:47 -0800

    Lower log level to error

--- stream.sh
+++ stream.sh
@@ -3,5 +3,5 @@
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
-/usr/bin/ffserver -hide_banner -loglevel fatal &
-/usr/bin/ffmpeg -hide_banner -loglevel fatal -rtsp_transport tcp -i ${RTSP_URL} http://127.0.0.1:8090/feed.ffm
+/usr/bin/ffserver -hide_banner -loglevel error &
+/usr/bin/ffmpeg -hide_banner -loglevel error -rtsp_transport tcp -i ${RTSP_URL} http://127.0.0.1:8090/feed.ffm
