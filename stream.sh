cb6cd53178a4f4fae9d9da683c065e2528f24177
eroji <jimmy.chen@outlook.com>
2020-01-09 10:30:32 -0800

    Support input/output opts

--- stream.sh
+++ stream.sh
@@ -4,4 +4,4 @@ trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
 /usr/bin/ffserver -hide_banner -loglevel ${FFSERVER_LOG_LEVEL} &
-/usr/bin/ffmpeg -hide_banner -loglevel ${FFMPEG_LOG_LEVEL} -rtsp_transport tcp -i ${RTSP_URL} ${FFMPEG_PARAM} http://127.0.0.1:8090/feed.ffm
+/usr/bin/ffmpeg -hide_banner -loglevel ${FFMPEG_LOG_LEVEL} -rtsp_transport tcp ${FFMPEG_INPUT_OPTS} -i ${RTSP_URL} ${FFMPEG_OUPUT_OPTS} http://127.0.0.1:8090/feed.ffm
