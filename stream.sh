cd8dbda8abf5f316c9eec7567145142e4de6e707
eroji <jimmy.chen@outlook.com>
2020-01-11 00:41:38 -0800

    Compile ffmpeg for armhf image with rpi omax driver

--- stream.sh
+++ stream.sh
@@ -3,5 +3,5 @@
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
-/usr/bin/ffserver -hide_banner -loglevel ${FFSERVER_LOG_LEVEL} &
-/usr/bin/ffmpeg -hide_banner -loglevel ${FFMPEG_LOG_LEVEL} -rtsp_transport tcp ${FFMPEG_INPUT_OPTS} -i ${RTSP_URL} ${FFMPEG_OUTPUT_OPTS} http://127.0.0.1:8090/feed.ffm
+ffserver -hide_banner -loglevel ${FFSERVER_LOG_LEVEL} &
+ffmpeg -hide_banner -loglevel ${FFMPEG_LOG_LEVEL} -rtsp_transport tcp ${FFMPEG_INPUT_OPTS} -i ${RTSP_URL} ${FFMPEG_OUTPUT_OPTS} http://127.0.0.1:8090/feed.ffm
